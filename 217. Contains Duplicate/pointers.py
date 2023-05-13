1395ea2f61e4650a046919e41c982eb2ac1cb9df
Gabriel Volpato <gvolpatolima@gmail.com>
2023-05-13 18:52:56 -0300

    pointer method

---
+++ 217. Contains Duplicate/pointers.py
@@ -0,0 +1,14 @@
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        nums.sort()
+        n = len(nums)
+        left = 0
+        right = 1
+
+        while right < n:
+            if nums[left] == nums[right]:
+                return True
+            left += 1
+            right += 1
+
+        return False
